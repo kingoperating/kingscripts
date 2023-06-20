ba3f4e7fcd9a5ca71343e851b828908c9dd447b9
mtanner161 <m.tanner161@gmail.com>
2023-06-20 10:42:58 -0500

    adding kingscripts repo

---
+++ run.py
@@ -0,0 +1,6 @@
+from kingscripts import enverus as env
+
+data = env.getWellProductionData(
+    "DbMGvz0WhBgz9a5MNEbzlzK_B0YlHwp3ow6tc7AW31i45856daynEFFKwyrRDV5V", "42461411020000")
+
+print("yay")
